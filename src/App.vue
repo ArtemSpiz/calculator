e522b291447cbca2e00d1f62ac8426050b57efa5
unknown <artemijspizhav@gmail.com>
2025-05-25 17:29:52 +0300

    ''

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,31 @@
 <script setup>
 import Home from '@/pages/Home.vue'
+import { onMounted } from 'vue'
+
+onMounted(() => {
+	document.addEventListener(
+		'touchstart',
+		event => {
+			if (event.touches.length > 1) {
+				event.preventDefault()
+			}
+		},
+		{ passive: false }
+	)
+
+	let lastTouchEnd = 0
+	document.addEventListener(
+		'touchend',
+		event => {
+			const now = Date.now()
+			if (now - lastTouchEnd <= 300) {
+				event.preventDefault()
+			}
+			lastTouchEnd = now
+		},
+		{ passive: false }
+	)
+})
 </script>
 
 <template>
